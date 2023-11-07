51e7625f94214fbb538a254466539e07f834e2c5
neharvard <ummeneha99@.com>
2023-11-07 10:36:42 +0600

    add the initial setup and navbar

---
+++ src/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            {/* <h2>This is Home</h2> */}
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,108 @@
+import { NavLink } from "react-router-dom";
+// import { useContext } from "react";
+// import { AuthContext } from "../../../Providers/AuthProvider";
+import { FiSun, FiMoon } from "react-icons/fi";
+// import ThemeContext, { useTheme } from "../../../Theme/ThemeContext";
+
+//import { AuthContext } from "../../../Providers/AuthProvider";
+
+const Navbar = () => {
+
+     //const { theme, toggleTheme } = useTheme(); // Use the useTheme hook
+    // const { toggleTheme } = useTheme(); // Use the useTheme hook
+    // console.log('navbarTheme: ', toggleTheme);
+    // const { theme, toggleTheme } = useContext(ThemeContext);
+
+    
+    // const { user, logOut } = useContext(AuthContext);
+    // const handleLogOut = () => {
+    //     logOut()
+    //         .then()
+    //         .catch()
+    // }
+    const navLinks = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        {/* <li><NavLink to='/services'>Services</NavLink></li> */}
+        {/* <li><a href="/#addProduct">Add Product</a></li> */}
+        <li><NavLink to="/addProduct">Add Blog</NavLink></li>
+        <li><NavLink to="/addProduct">All blogs</NavLink></li>
+        <li><NavLink to="/addProduct">Featured Blogs </NavLink></li>
+        <li><NavLink to='/cart'>Wishlist</NavLink></li>
+        {/* <li><NavLink to='/login'>Login</NavLink></li> */}
+    </>
+
+    return (
+        <div className="navbar bg-base-100 mt-7">
+            {/* <h2>THIS is Navar</h2> */}
+            <div className="navbar-start">
+                <div className="flex gap-5">
+                    {/* <FaBloggerB></FaBloggerB> */}
+                    <p className="text-3xl ml-8 font-extrabold italic font-serif">
+                        <span className="text-pink-400">B</span>log Express</p>
+                </div>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLinks}
+                    </ul>
+                </div>
+
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img src='https://i.ibb.co/Q6KWgsc/user.png' />
+                    </div>
+                </label>
+                {/* {
+                    user ?
+                        <button
+                            onClick={handleLogOut}
+                            className="btn">Log Out</button>
+                        :
+                        <Link to='/login'>
+                            <button className="btn">Login</button>
+                        </Link>
+                } */}
+                <button className="btn">Login</button>
+
+                {/* new add */}
+                <button //onClick={toggleTheme} 
+                className="ml-4 btn btn-circle theme-toggle">
+                    {/* {theme === "dark" ? <FiSun /> : <FiMoon />} */}
+                    <FiSun />  <FiMoon />
+                </button>
+                {/* <button onClick={toggleTheme} className="btn btn-circle theme-toggle">
+                    Toggle Theme
+                </button> */}
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
