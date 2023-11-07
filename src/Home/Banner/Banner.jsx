c9f3f21769e35aa2b7d0f523d5d194255a9ff710
neharvard <ummeneha99@.com>
2023-11-07 10:38:26 +0600

    add the banner section

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -2,7 +2,17 @@
 
 const Banner = () => {
     return (
-       <div></div>
+        <div className="hero min-h-screen mt-10" style={{ backgroundImage: 'url(https://i.ibb.co/WWrg4Gw/Blogging.jpg)' }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                <p className="mb-5">Welcome to a World of Endless Inspiration. Explore, Learn, and Share with Our Blog.</p>
+                {/* <button className="btn btn-primary">Get Started</button> */}
+            </div>
+        </div>
+    </div>
+
     );
 };
 
